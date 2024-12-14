96cdb7231aac117e6cda38d2bca6c5246f27f3bf
BanditSunspot <65348913+BanditSunspot@users.noreply.github.com>
2024-12-14 12:35:43 +0100

    Change discord embeb publish with a helmet balise edit text

--- src/App.js
+++ src/App.js
@@ -11,21 +11,15 @@ import { EvenementsMover } from './Components/EvenementsManager';
 class App extends Component {
   DisplayWeb(){
     return(
-      <div className='application'>
-        <Helmet>
-          <title>Learning React Helmet!</title>
-          <meta name='description' content='Beginner friendly page for learning React Helmet.' />
-        </Helmet>
-        <Router>
-          <Routes>
-            <Route element={<Layout/>}>
-              <Route path="/" element={<Acceuil/>}/>
-              <Route path="/Boutique" element={<Boutique/>}/>
-              <Route path="/ViewProduct/:Id" element={<ViewProduct/>}/>
-            </Route>
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Acceuil/>}/>
+            <Route path="/Boutique" element={<Boutique/>}/>
+            <Route path="/ViewProduct/:Id" element={<ViewProduct/>}/>
+          </Route>
+        </Routes>
+      </Router>
     );
   }
 
